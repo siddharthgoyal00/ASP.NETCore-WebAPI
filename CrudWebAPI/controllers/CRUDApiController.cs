368556e594325a46a0785e9f2859b630d5fae661
siddharth goyal <113788213+siddharthgoyal00@users.noreply.github.com>
2024-07-10 16:55:51 +0530

    web api

--- CrudWebAPI/controllers/CRUDApiController.cs
+++ CrudWebAPI/controllers/CRUDApiController.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using MyApiProject.Data;
+using MyApiProject.Models;
 
 namespace CrudWebAPI.controllers
 {
--- MyCSharpApp/obj/Debug/net8.0/MyCSharpApp.AssemblyInfo.cs
+++ MyCSharpApp/obj/Debug/net8.0/MyCSharpApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyCSharpApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac830519f1dacc3d261cc1b7ab26ed8613a44aee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7bebb20a73de5eb88e0bb3b3ca4a86760190d78")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyCSharpApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyCSharpApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
